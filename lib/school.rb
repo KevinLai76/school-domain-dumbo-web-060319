31ed79572b287b8009f51ddfb2375f84e7f5ff7b
Kevin <kevinlai76@gmail.com>
2019-06-06 23:52:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade].push(name)
+  end 
+  
+  
+  
+end
